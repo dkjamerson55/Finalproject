7b01cc711ac845667c95c4c1b3fb8f0be7f44779
dkjamerson55 <derekkj_55@hotmail.com>
2023-05-28 16:03:19 -0400

    finished router links

--- react-router boiler plate/src/App.js
+++ react-router boiler plate/src/App.js
@@ -1,13 +1,31 @@
 import React from 'react';
+import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
+import Home from './Components/Home';
+import Users from './Components/Users';
+import Posts from './Components/Posts';
 import './App.css';
 
 
 export default function App() {
   return (
-    <div>
-      
-      <h1>Hello World 🌎</h1>
-      
-    </div>
+    <Router>
+      <div className='container'>
+        <h1>Hello World 🌎</h1>
+        <nav className='navbar'>
+          <ul className='navbar-nav'>
+            <li><Link to ={'/'} className='nav-link'>Home</Link></li>
+            <li><Link to ={'/users'} className='nav-link'>Users</Link></li>
+            <li><Link to ={'/posts'} className='nav-link'>Posts</Link></li>
+          </ul>
+        </nav>
+        <hr />
+        <Switch>
+          <Route exact path='/' component={Home}/>
+          <Route path='/users' component={Users}/>
+          <Route path='/posts' component={Posts}/>
+        </Switch>
+      </div>
+    </Router>
+    
   );
 }
--- react-router boiler plate/src/Components/Users.js
+++ react-router boiler plate/src/Components/Users.js
@@ -1,16 +1,16 @@
 import React from 'react'
 
 function Users(props) {
-  const USER_API = https://64412ead792fe886a8a09b3d.mockapi.io/wk12API/users
-  const {names} = props;
+  // const USER_API = https://64412ead792fe886a8a09b3d.mockapi.io/wk12API/users
+  // const {names} = props;
   return (
     <div>
-      <ul> 
+      {/* <ul> 
         {/* map over names array and for each user element pass index to keep track & return an li with name of user. */}
-        {names.map((user, index) => (
+        {/* {names.map((user, index) => (
           <li key={index}>{friend}</li>
         ))}
-      </ul>
+      // </ul> */} Users
     </div>
   )
 }
